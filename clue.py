fc49638a16b3f19a6683b739285e67ae7bffe65e
Alex Lucchesi <aplucche@gmail.com>
2026-09-11 15:23:15 -0400

    Add custom scorer and sandbox

--- clue.py
+++ clue.py
@@ -1,12 +1,23 @@
 
 import json
 import random
-from typing import Literal
+from typing import Literal, cast
 from inspect_ai.tool import tool
 from inspect_ai.tool import bash, todo_write
 from inspect_ai import Task, task
 from inspect_ai.agent import react
+from inspect_ai.dataset import Sample
 from itertools import product
+from inspect_ai.solver import TaskState
+from inspect_ai.scorer import (
+    CORRECT,
+    INCORRECT,
+    Score,
+    Target,
+    accuracy,
+    scorer,
+    stderr,
+)
 
 type ClueType = Literal['weapon', 'suspect', 'room']
 
@@ -18,19 +29,11 @@ combinations = list(product(weapons,suspects,rooms))
 
 try:
     with open('answer.json', 'r') as answer_file:
-        answer = json.load(answer_file)
+        answer = cast(list[str], json.load(answer_file))
 except:
     answer = [random.choice(weapons), random.choice(suspects), random.choice(rooms)] 
     with open('answer.json' , 'w') as answer_file:
         json.dump(answer, answer_file)
-
-
-print(answer)
-filtertest = list(filter(lambda x: x[0] == 'Candlestick', combinations))
-#filtertest = [item for item in p if item[0]=='Candlestick' ]
-
-for f in filtertest:
-    print(f)
 
 def correct_clue(clue_type: ClueType) -> list[str]:
     if clue_type=='weapon': 
@@ -46,7 +49,6 @@ def get_clue(suspect: str) -> list[str]:
     else:
         return answer
 
-
 @tool
 def ask_suspect_for_clue():
     async def execute(suspect_name: str) -> str:
@@ -60,19 +62,24 @@ def ask_suspect_for_clue():
             A clue - true if the suspect is innocent, random if guilty
         """
         return ', '.join(get_clue(suspect_name))
-
     return execute
 
+# https://inspect.aisi.org.uk/custom-scorers.html
+@scorer(metrics=[accuracy(), stderr()])
+def includes_all():
+    async def score(state: TaskState, target: Target) -> Score:
+        correct = all(item.lower() in state.output.completion.lower() for item in target)
+        return Score(value=CORRECT if correct else INCORRECT, answer=state.output.completion)
+    return score
 
 @task
 def cluetest():
     return Task(
-        solver= react(
-            prompt=(
-                    "You are a game playing agent playing a variation of the classic game clue. Use tools to solve the mystery."
-                ),
-        tools=[bash(), todo_write(), ask_suspect_for_clue() ]
+        dataset=[Sample(input="Who dunnit?", target=[', '.join(answer)])],
+        solver=react(
+            prompt=("You are a game playing agent playing a variation of the classic game clue. Use tools to solve the mystery."),
+            tools=[bash(), todo_write(), ask_suspect_for_clue() ]
             ),
+        scorer=includes_all(),
+        sandbox='docker'
     )
-
-
